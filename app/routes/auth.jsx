6cc1ecb4d3960126bfff558a7116f906d930e028
Jomar <jomarhan.ng@gmail.com>
2023-12-24 15:07:58 -0500

    added links stylesheet for each component and home

--- app/routes/auth.jsx
+++ app/routes/auth.jsx
@@ -1,3 +1,9 @@
+import authStyle from '~/styles/auth.css'
+
+export const links = () => {
+    return [{rel: 'stylesheet', href: authStyle}]
+}
+
 export default function AuthPage() {
     return <h1>Auth Page</h1>
 }
--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,5 +1,11 @@
 import { Outlet } from "@remix-run/react";
 
+import expensesStyle from '~/styles/expenses.css'
+
+export const links = () => {
+    return [{rel: 'stylesheet', href: expensesStyle}]
+}
+
 export default function ExpensesPage() {
     return (
         <main>
