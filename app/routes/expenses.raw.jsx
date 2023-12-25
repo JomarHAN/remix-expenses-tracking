8947149aa0280b3574a19b2e0a4927dab0d94bd1
Jomar <jomarhan.ng@gmail.com>
2023-12-25 17:25:08 -0500

    added codes for expenses.raw.jsx

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,3 +1,18 @@
-export default function ExpensesRawPage() {
-    return <h1>Expenses Raw Page</h1>
-}
+const DUMMY_DATA = [
+  {
+    id: "e1",
+    title: "First title",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second title",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export const loader = () => {
+  return DUMMY_DATA;
+};
