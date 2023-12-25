61b30129be07b64bff761b188dbb0aac892e5b99
Jomar <jomarhan.ng@gmail.com>
2023-12-25 18:24:48 -0500

    add AddNote Button and DownloadRawData button

--- app/routes/expenses._index.jsx
+++ app/routes/expenses._index.jsx
@@ -1,3 +1,5 @@
+import { Link } from "@remix-run/react";
+import { FaDownload, FaPlus } from "react-icons/fa";
 import ExpensesList from "~/components/expenses/ExpensesList";
 
 const DUMMY_DATA = [
@@ -18,6 +20,16 @@ const DUMMY_DATA = [
 export default function ExpensesIndex() {
   return (
     <main>
+      <section id="expenses-actions">
+        <Link to={"add"}>
+          <FaPlus />
+          <span>Add Expense</span>
+        </Link>
+        <a href="/expenses/raw">
+          <FaDownload />
+          <span>Download Raw Data</span>
+        </a>
+      </section>
       <ExpensesList expenses={DUMMY_DATA} />
     </main>
   );
