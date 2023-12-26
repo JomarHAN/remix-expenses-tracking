277349cb02b160ea8b6546f1da2c71e1ea47ae92
Jomar <jomarhan.ng@gmail.com>
2023-12-26 15:37:38 -0500

    added validation function into action

--- app/routes/expenses.add.jsx
+++ app/routes/expenses.add.jsx
@@ -2,6 +2,7 @@ import Modal from "~/components/util/Modal";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import { redirect, useNavigate } from "@remix-run/react";
 import { addExpenses } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 export default function ExpensesAddPage() {
   const navigate = useNavigate();
@@ -18,6 +19,12 @@ export default function ExpensesAddPage() {
 export async function action({ request }) {
   const formData = await request.formData();
   const expensesForm = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expensesForm);
+  } catch (error) {
+    return error;
+  }
 
   await addExpenses(expensesForm);
   return redirect("/expenses");
