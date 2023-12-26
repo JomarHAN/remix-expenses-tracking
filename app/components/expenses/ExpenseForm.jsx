b4ce2430a176cbc426880b2781f4a96392e42e16
Jomar <jomarhan.ng@gmail.com>
2023-12-26 17:18:16 -0500

    load expense data from DB

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,17 +1,41 @@
-import { Form, Link, useActionData, useNavigation } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
   const errorAction = useActionData();
+  let defaultValue = {};
 
   const navigation = useNavigation();
   const isSubmitting = navigation.state != "idle";
+
+  const loaderData = useLoaderData();
+
+  defaultValue = loaderData
+    ? {
+        title: loaderData.title,
+        amount: loaderData.amount,
+        date: loaderData.date.slice(0, 10),
+      }
+    : { title: "", amount: "", date: "" };
 
   return (
     <Form method="post" className="form" id="expense-form">
       <p>
         <label htmlFor="title">Expense Title</label>
-        <input type="text" id="title" name="title" required maxLength={30} />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          maxLength={30}
+          defaultValue={defaultValue.title}
+        />
       </p>
 
       <div className="form-row">
@@ -24,11 +48,19 @@ function ExpenseForm() {
             min="0"
             step="0.01"
             required
+            defaultValue={defaultValue.amount}
           />
         </p>
         <p>
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" max={today} required />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            max={today}
+            required
+            defaultValue={defaultValue.date}
+          />
         </p>
       </div>
       {errorAction && (
