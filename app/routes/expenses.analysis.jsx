a00e31892a922830e899dfb44b093913d5ab58f4
Jomar <jomarhan.ng@gmail.com>
2023-12-27 15:34:51 -0500

    fetch data and errorBoundary for analysis

--- app/routes/expenses.analysis.jsx
+++ app/routes/expenses.analysis.jsx
@@ -1,26 +1,48 @@
+import {
+  Link,
+  isRouteErrorResponse,
+  json,
+  useLoaderData,
+  useRouteError,
+} from "@remix-run/react";
 import Chart from "~/components/expenses/Chart";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
-
-const DUMMY_DATA = [
-  {
-    id: "e1",
-    title: "First title",
-    amount: 12.99,
-    date: new Date().toISOString(),
-  },
-  {
-    id: "e2",
-    title: "Second title",
-    amount: 16.99,
-    date: new Date().toISOString(),
-  },
-];
+import Error from "~/components/util/Error";
+import { getExpenses } from "~/data/expenses.server";
 
 export default function ExpensesAnalysisPage() {
+  const expenses = useLoaderData();
+
   return (
     <main>
-      <Chart expenses={DUMMY_DATA} />
-      <ExpenseStatistics expenses={DUMMY_DATA} />
+      <Chart expenses={expenses} />
+      <ExpenseStatistics expenses={expenses} />
     </main>
   );
 }
+
+export const loader = async () => {
+  const expenses = await getExpenses();
+
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      { message: "No Existing Expenses Found!" },
+      { status: 404, statusText: "No Expenses" }
+    );
+  }
+  return expenses;
+};
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+  if (error) {
+    return (
+      <main>
+        <Error title={error.statusText}>
+          <p>{error.data?.message}</p>
+          <Link to={"/expenses/add"}>Add a new Expense</Link>
+        </Error>
+      </main>
+    );
+  }
+};
