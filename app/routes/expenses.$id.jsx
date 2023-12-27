c62ad9eb900058a1ef7f3f8aef3bbecc1430f288
Jomar <jomarhan.ng@gmail.com>
2023-12-26 19:48:54 -0500

    added DELETE function to FE

--- app/routes/expenses.$id.jsx
+++ app/routes/expenses.$id.jsx
@@ -1,7 +1,11 @@
 import { redirect, useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
-import { getExpenseById, updateExpenseById } from "~/data/expenses.server";
+import {
+  deleteExpense,
+  getExpenseById,
+  updateExpenseById,
+} from "~/data/expenses.server";
 import { validateExpenseInput } from "~/data/validation.server";
 
 export default function ExpensesUpdatePage() {
@@ -25,11 +29,15 @@ export const action = async ({ params, request }) => {
   const formData = await request.formData();
   const expenseUpdateForm = Object.fromEntries(formData);
 
-  try {
-    validateExpenseInput(expenseUpdateForm);
-  } catch (error) {
-    return error;
+  if (request.method === "PATCH") {
+    try {
+      validateExpenseInput(expenseUpdateForm);
+    } catch (error) {
+      return error;
+    }
+    await updateExpenseById(expenseId, expenseUpdateForm);
+  } else if (request.method === "DELETE") {
+    await deleteExpense(expenseId);
   }
-  await updateExpenseById(expenseId, expenseUpdateForm);
   return redirect("/expenses");
 };
