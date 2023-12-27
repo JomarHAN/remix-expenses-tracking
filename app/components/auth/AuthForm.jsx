2b97003d727981faa2fdb74cce4d3b629d090654
Jomar <jomarhan.ng@gmail.com>
2023-12-27 13:11:45 -0500

    add loading status UI for login button

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,16 +1,18 @@
-import { Link, useSearchParams } from "@remix-run/react";
+import { Form, Link, useNavigation, useSearchParams } from "@remix-run/react";
 import { FaLock, FaUserPlus } from "react-icons/fa";
 
 function AuthForm() {
   const [searchParams] = useSearchParams();
   const authMode = searchParams.get("mode") || "login";
+  const navigation = useNavigation();
 
+  const isSubmitting = navigation.state !== "idle";
   const submitBtnCaption = authMode === "login" ? "Login" : "Create New User";
   const toggleBtnCaption =
     authMode === "login" ? "Create a new user" : "Log in with existing user";
 
   return (
-    <form method="post" className="form" id="auth-form">
+    <Form method="post" className="form" id="auth-form">
       <div className="icon-img">
         {authMode === "login" ? <FaLock /> : <FaUserPlus />}
       </div>
@@ -23,12 +25,14 @@ function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>{submitBtnCaption}</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Authenticating..." : submitBtnCaption}
+        </button>
         <Link to={authMode === "login" ? "?mode=signin" : "?mode=login"}>
           {toggleBtnCaption}
         </Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
