86eae5bf83ecbbeac51461bd2405787d4ee5ddde
Jomar <jomarhan.ng@gmail.com>
2023-12-27 13:13:29 -0500

    Use shared Document for App and ErrorBoundary

--- app/root.jsx
+++ app/root.jsx
@@ -1,14 +1,17 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
 
 import sharedStyle from "~/styles/shared.css";
+import Error from "./components/util/Error";
 
 export const links = () => [
   ...(cssBundleHref
@@ -16,13 +19,15 @@ export const links = () => [
     : [{ rel: "stylesheet", href: sharedStyle }]),
 ];
 
-export default function App() {
+export const Document = ({ title, children }) => {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+
+        <title>{title}</title>
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
@@ -38,11 +43,51 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
   );
+};
+
+export default function App() {
+  return (
+    <Document title="Remix Expenses Tracking">
+      <Outlet />
+    </Document>
+  );
 }
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (error) {
+    return (
+      <Document title={error.message}>
+        <main>
+          <Error title={error.status}>
+            {error.message}
+            <p>
+              Back to <Link to="/">Home</Link>
+            </p>
+          </Error>
+        </main>
+      </Document>
+    );
+  }
+
+  return (
+    <Document title={`Something went wrong`}>
+      <main>
+        <Error title={`Something went wrong`}>
+          <p>Something went wrong. Please try again later!</p>
+          <p>
+            Back to <Link to="/">Home</Link>
+          </p>
+        </Error>
+      </main>
+    </Document>
+  );
+};
